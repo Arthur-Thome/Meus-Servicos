44ef913e48debf2e6bb7318910616dae648d2fd3
Arthur de Azevedo Thome <arthur.thome@sulmed.com.br>
2024-09-27 17:03:47 -0300

    Página inicial pronta

---
+++ js/index.js
@@ -0,0 +1,4 @@
+// Script para alternar a visibilidade do menu em dispositivos móveis
+document.getElementById('menu-toggle').addEventListener('click', function() {
+document.querySelector('.menu').classList.toggle('active');
+});
